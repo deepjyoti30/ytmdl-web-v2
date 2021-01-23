532d59ba9ecaa942621fb402eef87760b7cbe96e
Deepjyoti Barman <deep.barman30@gmail.com>
2021-01-23 22:41:57 +0530

    Add proper code to read and render the markdown to html

--- src/components/StaticContent.vue
+++ src/components/StaticContent.vue
@@ -1,9 +1,43 @@
 <template>
-  <div class="static__container"></div>
+  <div class="static__container">
+    <div class="content" v-html="getContent"></div>
+  </div>
 </template>
 
 <script>
+import marked from "marked";
+
 export default {
-  name: "StaticContent"
+  name: "StaticContent",
+  props: {
+    filePath: {
+      type: String
+    }
+  },
+  data: () => {
+    return {
+      content: ""
+    };
+  },
+  methods: {
+    readContent: function() {
+      /**
+       * Read the content of the passed file based on
+       * the path and accordingly store it in a local
+       * variable.
+       */
+      this.$http.get(this.filePath).then(response => {
+        this.content = marked(response.body);
+      });
+    }
+  },
+  computed: {
+    getContent() {
+      return this.content;
+    }
+  },
+  mounted() {
+    this.readContent();
+  }
 };
 </script>
