f1432440b1f60e9da52d48bdb70379d390d7bf18
Deepjyoti Barman <deep.barman30@gmail.com>
2021-01-23 14:29:20 +0530

    Add some fixes for the old navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Navbar />
     <router-view />
   </div>
 </template>
+
+<script>
+import Navbar from "@/components/Navbar";
+
+export default {
+  name: "App",
+  components: {
+    Navbar
+  }
+};
+</script>
 
 <style lang="scss"></style>
