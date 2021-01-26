fb77af11804fb88802bcb2ca0cb1f0c2ef091c1c
Deepjyoti Barman <deep.barman30@gmail.com>
2021-01-26 21:08:05 +0530

    Add some more styling for the search bar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -8,24 +8,40 @@
     <div
       class="divider--line w-0 mr-auto ml-auto border border-gray-200 dark:border-gray-700 h-7"
     ></div>
-    <div class="search--box shadow-cust w-2/5 mb-2 mr-auto ml-auto rounded-md">
+    <div
+      class="search--box shadow-cust w-2/5 mb-2 mr-auto ml-auto rounded-lg flex"
+    >
+      <SearchIcon size="1.5x" class="search-icon mt-4" />
       <input
         type="text"
         class="w-full my-2 border py-2 pl-4 text-xl"
         placeholder="Save your tears"
+        spellcheck="false"
+        autocomplete="false"
       />
     </div>
   </div>
 </template>
 
 <script>
+import { SearchIcon } from "vue-feather-icons";
+
 export default {
-  name: "SearchBar"
+  name: "SearchBar",
+  components: {
+    SearchIcon
+  }
 };
 </script>
 
 <style lang="scss" scoped>
 input {
   @extend .strip-border;
+  background: none;
+}
+
+.search-icon {
+  transform: translateX(0.5rem);
+  color: $grey-lighter;
 }
 </style>
