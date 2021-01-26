dc3a961c0403c5d6665ae6f80a9e3c61d1ed0f33
Deepjyoti Barman <deep.barman30@gmail.com>
2021-01-26 20:27:28 +0530

    Add basic text in the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,15 @@
 <template>
-  <div></div>
+  <div class="home__container py-16">
+    <div class="top__container text-center">
+      <h1 class="text-4xl font-bold">
+        Download songs with metadata in the best quality possible!
+      </h1>
+      <p class="my-8 text-gray-600 dark:text-gray-300">
+        Ytmdl lets you download songs with metadata embedded from sources like
+        Itunes, LastFM etc in the highest quality possible.
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,3 +18,13 @@ export default {
   components: {}
 };
 </script>
+
+<style lang="scss" scoped>
+.home__container {
+  .top__container {
+    h1 {
+      color: $lightgreen;
+    }
+  }
+}
+</style>
--- tailwind.js
+++ tailwind.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {}
   },
