397834fcfe8a232a1fa045e76fe3b29cb2441c9a
Deepjyoti Barman <deep.barman30@gmail.com>
2021-01-26 22:01:59 +0530

    Fix the position of the clear and the search icon

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -9,15 +9,16 @@
       class="divider--line w-0 mr-auto ml-auto border border-gray-200 dark:border-gray-700 h-7"
     ></div>
     <div
-      class="search--box shadow-cust md:w-2/5 w-11/12 mb-2 mr-auto ml-auto rounded-lg flex"
+      class="search--box shadow-cust md:w-2/5 w-11/12 mb-2 mr-auto ml-auto rounded-lg"
     >
       <SearchIcon size="1.5x" class="search-icon mt-4" />
       <input
         type="text"
-        class="w-full my-2 border py-2 pl-4 text-xl"
+        class="w-full my-2 border py-2 pl-12 pr-12 text-xl"
         placeholder="Save your tears"
         spellcheck="false"
         autocomplete="false"
+        v-model="songEntered"
       />
       <button type="button" class="strip-button" title="Clear Input">
         <XCircleIcon v-if="getShowClearIcon" size="1.5x" class="clear-icon" />
@@ -37,12 +38,12 @@ export default {
   },
   data: () => {
     return {
-      showClearIcon: false
+      songEntered: ""
     };
   },
   computed: {
     getShowClearIcon() {
-      return this.showClearIcon;
+      return this.songEntered.length;
     }
   }
 };
@@ -56,6 +57,11 @@ input {
 
 .icon {
   color: $grey-lighter;
+  position: absolute;
+}
+
+.search--box {
+  position: relative;
 }
 
 .search-icon {
@@ -66,8 +72,7 @@ input {
 
 .clear-icon {
   @extend .icon;
-  transform: translateX(-0.5rem);
-
-  cursor: find;
+  top: 1rem;
+  right: 0.5rem;
 }
 </style>
