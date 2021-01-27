ea2a6c5a8450abc04c018e1409756a29266784da
Deepjyoti Barman <deep.barman30@gmail.com>
2021-01-27 17:31:44 +0530

    Add some more settings for the user

--- src/static/settings.js
+++ src/static/settings.js
@@ -14,6 +14,22 @@ const defaultSettings = [
       "If enabled, clicking on the search result will mean continue. This makes the flow getting the song ready easier since it removes some extra clicks that the user might have had to do.",
     options: ["on", "off"],
     default: "off"
+  },
+  {
+    name: "ask-format-each",
+    title: "Ask for format each time",
+    description:
+      "Ask the user which format they'd like to download the song in, each time. If disabled, the song will be downloaded in the format selected by the user which is by default set to mp3. This setting will have not effect if the default format is not selected.",
+    options: ["on", "off"],
+    default: "on"
+  },
+  {
+    name: "theme",
+    title: "Theme",
+    description:
+      "Theme of the app overall. This will save the theme of the app which will be saved through refresshes. There's a shorthand theme button on the navbar as well",
+    options: ["dark", "light"],
+    default: "light"
   }
 ];
 
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="settings__container">
-    <div class="settings--content w-3/5 mr-auto ml-auto mt-16">
+    <div class="settings--content w-3/5 mr-auto ml-auto mt-16 mb-24">
       <div class="settings--text">
         <h1 class="font-semibold text-4xl dark:text-white">Settings</h1>
       </div>
