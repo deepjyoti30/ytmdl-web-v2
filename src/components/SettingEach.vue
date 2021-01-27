8a4d450cd228fd050ca988fa0ff9fe92a9d58d34
Deepjyoti Barman <deep.barman30@gmail.com>
2021-01-27 16:35:33 +0530

    Add a space for the description of the setting

--- src/components/SettingEach.vue
+++ src/components/SettingEach.vue
@@ -1,11 +1,29 @@
 <template>
   <div class="setting--each__container">
-    <div class="setting--each py-4 my-2 flex justify-between">
-      <div class="setting--text">
-        <h2 class="text-2xl font-medium text-gray-600">Format</h2>
+    <div class="setting--each py-4 my-2 flex justify-between items-center">
+      <div class="setting--text w-2/4">
+        <h2 class="text-2xl font-medium text-gray-700 dark:text-gray-400">
+          Format
+        </h2>
+        <div class="description text-md mt-4 text-gray-600 dark:text-gray-500">
+          Format is the format in which the song will be downloaded in. mp3 is
+          the older but famous format. m4a is a good format. Takes less space
+          and songs are good quality.
+        </div>
       </div>
       <div class="setting--handler">
-        <button type="button" class="px-4 py-2 rounded-md">mp3</button>
+        <button
+          type="button"
+          class="px-4 py-2 rounded-md font-semibold text-gray-600"
+        >
+          mp3
+        </button>
+        <button
+          type="button"
+          class="px-4 py-2 rounded-md font-semibold text-gray-600"
+        >
+          m4a
+        </button>
       </div>
     </div>
   </div>
@@ -19,9 +37,20 @@ export default {
 
 <style lang="scss" scoped>
 .setting--each__container {
+  .setting-text {
+    .description {
+      @extend .dm-sans;
+    }
+  }
   .setting--handler {
     button {
       background: $yellow;
+
+      @apply mr-2;
+
+      &:last-child {
+        @apply mr-0;
+      }
     }
   }
 }
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -2,7 +2,7 @@
   <div class="settings__container">
     <div class="settings--content w-3/5 mr-auto ml-auto mt-16">
       <div class="settings--text">
-        <h1 class="font-semibold text-4xl">Settings</h1>
+        <h1 class="font-semibold text-4xl dark:text-white">Settings</h1>
       </div>
       <div class="settings--content mt-12">
         <SettingEach />
