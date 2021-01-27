77ecf72b6644a74d9a73300f5588259f6b622a00
Deepjyoti Barman <deep.barman30@gmail.com>
2021-01-27 17:21:18 +0530

    Add a new click result to continue option

--- src/static/settings.js
+++ src/static/settings.js
@@ -6,6 +6,14 @@ const defaultSettings = [
       "Format is the format in which the song will be downloaded in. mp3 is the older but famous format. m4a is a good format. Takes less space and songs are good quality.",
     options: ["mp3", "m4a", "opus"],
     default: "mp3"
+  },
+  {
+    name: "click-search",
+    title: "Click on result to continue",
+    description:
+      "If enabled, clicking on the search result will mean continue. This makes the flow getting the song ready easier since it removes some extra clicks that the user might have had to do.",
+    options: ["on", "off"],
+    default: "off"
   }
 ];
 
