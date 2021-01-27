08579eac201b3ecc86a2432e22b6515fa63ea380
Deepjyoti Barman <deep.barman30@gmail.com>
2021-01-27 13:35:20 +0530

    Add a component to handle the setting of each

---
+++ src/components/SettingEach.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="setting--each__container">
+    <div class="setting--each py-4 my-2 flex justify-between">
+      <div class="setting--text">
+        <h2 class="text-2xl font-medium text-gray-600">Format</h2>
+      </div>
+      <div class="setting--handler">
+        <button type="button" class="px-4 py-2 rounded-md">mp3</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SettingEach"
+};
+</script>
+
+<style lang="scss" scoped>
+.setting--each__container {
+  .setting--handler {
+    button {
+      background: $yellow;
+    }
+  }
+}
+</style>
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,9 +1,23 @@
 <template>
-  <div class="settings"></div>
+  <div class="settings__container">
+    <div class="settings--content w-3/5 mr-auto ml-auto mt-16">
+      <div class="settings--text">
+        <h1 class="font-semibold text-4xl">Settings</h1>
+      </div>
+      <div class="settings--content mt-12">
+        <SettingEach />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import SettingEach from "@/components/SettingEach";
+
 export default {
-  name: "Settings"
+  name: "Settings",
+  components: {
+    SettingEach
+  }
 };
 </script>
