c4188cefb4efccfe7fc94e7d5937d4bc7575da33
Deepjyoti Barman <deep.barman30@gmail.com>
2021-01-28 11:01:51 +0530

    Add a start button in the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,36 @@
 <template>
-  <div class="home__container md:py-16 py-24">
+  <div class="home__container md:py-16 pt-32 pb-8">
     <div class="top__container text-center">
       <h1 class="md:text-4xl text-3xl font-bold">
         Download <span class="highlight">songs with metadata</span> in the best
         quality possible!
       </h1>
+      <h4
+        class="md:text-xl text-md md:px-0 px-6 mt-12 text-gray-500 font-medium"
+      >
+        Get a high quality song with metadata embedded into the file in just a
+        few seconds.
+      </h4>
     </div>
-    <div class="search-bar-content pt-16">
-      <SearchBar />
+    <div class="start-btn-content pt-32">
+      <div class="start--btn--wrapper text-center">
+        <router-link
+          to="/search"
+          class="start--btn px-16 py-6 shadow-xl text-2xl font-medium rounded-lg"
+          >Download a song</router-link
+        >
+      </div>
     </div>
     <Details />
   </div>
 </template>
 
 <script>
-import SearchBar from "@/components/SearchBar";
 import Details from "@/components/Details";
 
 export default {
   name: "Home",
-  components: { SearchBar, Details }
+  components: { Details }
 };
 </script>
 
@@ -32,6 +43,26 @@ export default {
       .highlight {
         border-bottom: 5px solid $yellow;
         transform: translateZ(5px);
+      }
+    }
+
+    h4 {
+      @extend .dm-sans;
+    }
+  }
+
+  .start-btn-content {
+    .start--btn {
+      @extend .dm-sans;
+      $color: $lightgreen;
+
+      background: $color;
+      color: white;
+      transition: 150ms ease;
+
+      &:hover {
+        transition: 150ms ease;
+        background: darken($color, 2);
       }
     }
   }
