6207b00d4f883ba53385d1e3f4c4eeb165dccafb
Deepjyoti Barman <deep.barman30@gmail.com>
2021-02-06 19:39:08 +0530

    Fix a bug with the meta result object link

--- src/components/MetaResult.vue
+++ src/components/MetaResult.vue
@@ -1,25 +1,26 @@
 <template>
   <div class="meta--result__container">
-    <router-link :to="getLink">
-      <div class="meta--result md:my-4 my-3 max-w-3xl mr-auto ml-auto p-2 flex">
-        <div class="meta--cover w-1/4">
-          <img :src="getCover" alt="" class="rounded-md" loading="lazy" />
-        </div>
-        <div class="meta--details w-3/4 md:px-8 pl-3 py-2">
-          <h3
-            class="text-3xl font-semibold title text-gray-600 dark:text-gray-200"
-          >
-            {{ getTitle }}
-          </h3>
-          <h4 class="artist mt-3 text-xl text-gray-600 dark:text-gray-400">
-            {{ getArtist }}
-          </h4>
-          <h5 class="duration text-gray-700 dark:text-gray-400">
-            {{ getDuration }}
-          </h5>
-          <h5 class="album mt-3 text-md text-gray-500">{{ getAlbum }}</h5>
-          <h5 class="release text-md text-gray-500">{{ getRelease }}</h5>
-        </div>
+    <router-link
+      :to="getLink"
+      class="meta--result md:my-4 my-3 max-w-3xl mr-auto ml-auto p-2 flex"
+    >
+      <div class="meta--cover w-1/4">
+        <img :src="getCover" alt="" class="rounded-md" loading="lazy" />
+      </div>
+      <div class="meta--details w-3/4 md:px-8 pl-3 py-2">
+        <h3
+          class="text-3xl font-semibold title text-gray-600 dark:text-gray-200"
+        >
+          {{ getTitle }}
+        </h3>
+        <h4 class="artist mt-3 text-xl text-gray-600 dark:text-gray-400">
+          {{ getArtist }}
+        </h4>
+        <h5 class="duration text-gray-700 dark:text-gray-400">
+          {{ getDuration }}
+        </h5>
+        <h5 class="album mt-3 text-md text-gray-500">{{ getAlbum }}</h5>
+        <h5 class="release text-md text-gray-500">{{ getRelease }}</h5>
       </div>
     </router-link>
   </div>
