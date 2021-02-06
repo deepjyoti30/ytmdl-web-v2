bbad8f1a6f1912d82965b844f642fc5b0935e2c5
Deepjyoti Barman <deep.barman30@gmail.com>
2021-02-06 18:50:31 +0530

    Make the download result objects dynamic based on the content

--- src/components/DownloadButton.vue
+++ src/components/DownloadButton.vue
@@ -34,7 +34,7 @@ export default {
   },
   props: {
     downloadDetails: {
-      Object
+      type: Object
     }
   },
   methods: {
--- src/components/DownloadResult.vue
+++ src/components/DownloadResult.vue
@@ -2,7 +2,7 @@
   <div class="download--result__container md:flex">
     <div class="cover--img md:w-2/5 w-full">
       <img
-        src="https://is2-ssl.mzstatic.com/image/thumb/Music114/v4/9a/a0/eb/9aa0ebb8-3150-8f37-58ba-2329bd4bdd0a/source/480x480bb.jpg"
+        :src="getCover"
         alt=""
         loading="lazy"
         class="rounded-md shadow-2xl"
@@ -10,26 +10,26 @@
     </div>
     <div class="download--details py-12 md:px-12 px-2 md:w-3/5 w-full">
       <h1 class="title font-bold md:text-4xl text-3xl dark:text-white">
-        What You Know Bout Love
+        {{ getTitle }}
       </h1>
       <h3
         class="artist md:text-3xl text-2xl font-medium mt-6 text-gray-700 dark:text-gray-200"
       >
-        Pop Smoke
+        {{ getArtist }}
       </h3>
       <h4 class="genre font-medium text-gray-500 mt-4 dark:text-gray-400">
-        Hip-Hop/Rap
+        {{ getGenre }}
       </h4>
       <h4 class="release font-medium text-gray-500 mt-0.5 dark:text-gray-400">
-        2020-07-03
+        {{ getRelease }}
       </h4>
       <h3
         class="size text-xl font-medium mt-4 text-gray-600 dark:text-gray-400"
       >
-        5.66 MB
+        {{ getSize }}
       </h3>
       <div class="dw--btn mt-6">
-        <DownloadButton />
+        <DownloadButton :downloadDetails="getDownloadDetails" />
       </div>
     </div>
   </div>
@@ -49,6 +49,35 @@ export default {
     },
     downloadDetails: {
       type: Object
+    }
+  },
+  computed: {
+    getDownloadDetails() {
+      return this.downloadDetails;
+    },
+    getCover() {
+      // Check if the URL has something like 100x100, if it does, replace it with 480x480
+      var coverUrl = this.metaDetails.cover;
+      return coverUrl.replace("100x100", "480x480");
+    },
+    getTitle() {
+      return this.metaDetails.name;
+    },
+    getArtist() {
+      return this.metaDetails.artist;
+    },
+    getGenre() {
+      return this.metaDetails.genre;
+    },
+    getRelease() {
+      var release = this.metaDetails.release_date;
+      if (release.includes("T")) release = release.split("T")[0];
+
+      // Now convert the date to look more readable
+      return new Date(release).toLocaleString("en-US", { dateStyle: "medium" });
+    },
+    getSize() {
+      return this.downloadDetails.size.readable;
     }
   }
 };
