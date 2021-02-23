b2e67b9feeb0caaa5b9f9a0118cb7d3b83429d0c
Deepjyoti Barman <deep.barman30@gmail.com>
2021-02-23 19:34:49 +0530

    Add some fixes for the welcome features el

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -30,17 +30,22 @@
         </header>
         <main class="modal__content" id="welcome-frame-content">
           <div class="header--content text-center">
-            <h3 class="text-3xl font-semibold dark:text-white">
+            <h3 class="md:text-3xl text-xl font-semibold dark:text-white">
               Welcome to Ytmdl Web
               <span class="uppercase font-bold highlight-v2">v2</span>
             </h3>
             <p
-              class="mb-2 mt-8 max-w-2xl mr-auto ml-auto text-gray-600 dark:text-gray-400"
+              class="mb-2 mt-8 max-w-2xl md:text-base text-sm mr-auto ml-auto text-gray-600 dark:text-gray-400"
             >
               In even less than a year of the first release, v2 is here. This
               version is built from scratch with the consideration of various
               feedbacks provided by the users of the former version.
             </p>
+          </div>
+          <div class="features__container flex justify-around mt-6">
+            <welcome-feature text="Way faster">
+              <zap-icon />
+            </welcome-feature>
           </div>
         </main>
       </div>
@@ -50,12 +55,15 @@
 
 <script>
 import MicroModal from "micromodal";
-import { XIcon } from "vue-feather-icons";
+import WelcomeFeature from "@/components/WelcomeFeature.vue";
+import { XIcon, ZapIcon } from "vue-feather-icons";
 
 export default {
   name: "Welcome",
   components: {
-    XIcon
+    XIcon,
+    WelcomeFeature,
+    ZapIcon
   },
   methods: {
     showModal: function() {
---
+++ src/components/WelcomeFeature.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="welcome--feature__container">
+    <div class="welcome--feature p-4 rounded-xl bg-green-200 text-green-600">
+      <div class="icon--wrapper" align="center">
+        <slot></slot>
+      </div>
+      <h5 v-if="isText" class="text-md capitalize font-semibold mt-6">
+        {{ getText }}
+      </h5>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WelcomeFeature",
+  props: {
+    text: {
+      type: String,
+      default: null
+    }
+  },
+  computed: {
+    isText() {
+      return Boolean(this.text);
+    },
+    getText() {
+      return this.text;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.icon--wrapper {
+  svg {
+    width: 2.5em;
+    height: 2.5em;
+  }
+}
+</style>
