e9aeb550b570446fd02f94a3abfdd4c731d3cff1
Deepjyoti Barman <deep.barman30@gmail.com>
2021-02-25 10:27:34 +0530

    Add code to register service worker

--- src/main.js
+++ src/main.js
@@ -7,12 +7,12 @@ import "@/scss/_tailwind.scss";
 Vue.config.productionTip = false;
 
 // Check that service workers are supported
-/*if ("serviceWorker" in navigator) {
+if ("serviceWorker" in navigator) {
   // Use the window load event to keep the page load performant
   window.addEventListener("load", () => {
     navigator.serviceWorker.register("/sw.js");
   });
-}*/
+}
 
 new Vue({
   router,
