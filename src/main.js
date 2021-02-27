e5166deb0da36c8fcb02ff5010645dc919e015ca
Deepjyoti Barman <deep.barman30@gmail.com>
2021-02-27 14:06:18 +0530

    Add a function to refresh service worker on new content

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
+import workboxRefresh from "./refreshServiceWorker";
 import router from "./router";
 import "@/scss/_tailwind.scss";
 
@@ -13,6 +14,7 @@ if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("/sw.js");
   });
 }
+workboxRefresh();
 
 new Vue({
   router,
--- workbox-config.js
+++ workbox-config.js
@@ -2,7 +2,6 @@ module.exports = {
   globDirectory: "dist/",
   globPatterns: ["**/*.{ico,png,svg,html,js,css,json,txt,md}"],
   swDest: "dist/sw.js",
-  skipWaiting: true,
   runtimeCaching: [
     {
       urlPattern: /\.(?:png|jpg|jpeg|svg|json|md|js|css|html)$/,
