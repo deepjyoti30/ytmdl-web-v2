1441ed824debd1feeb4c615608793d472d80abce
Deepjyoti Barman <deep.barman30@gmail.com>
2021-02-27 14:19:59 +0530

    Try refreshing using the waiting handler

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 
 import { register } from "register-service-worker";
+import workboxRefresh from "./refreshServiceWorker";
 
 if (process.env.NODE_ENV === "production") {
   register(`${process.env.BASE_URL}service-worker.js`, {
@@ -21,6 +22,9 @@ if (process.env.NODE_ENV === "production") {
     },
     updated() {
       console.log("New content is available; please refresh.");
+    },
+    waiting() {
+      workboxRefresh();
     },
     offline() {
       console.log(
