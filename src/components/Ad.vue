91b46ff423535de5d2bf84a4307ccff98ee8bbc6
Deepjyoti Barman <deep.barman30@gmail.com>
2021-04-01 10:47:57 +0530

    Fix an issue with the ad size

--- src/components/Ad.vue
+++ src/components/Ad.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="ad--container">
-    <Adsense
-      data-ad-client="ca-pub-7702415526221680"
-      data-ad-slot="123456999"
-    ></Adsense>
+    <Adsense data-ad-client="ca-pub-7702415526221680"></Adsense>
   </div>
 </template>
 
