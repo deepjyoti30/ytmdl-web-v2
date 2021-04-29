256cec704935e48065c873e3d1f2fb7872a1d3c8
Deepjyoti Barman <deep.barman30@gmail.com>
2021-04-29 18:18:10 +0530

    Add some styles for the artist name

--- src/components/playlist/PlaylistSong.vue
+++ src/components/playlist/PlaylistSong.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="playlist--song__container w-3/4">
-    <div class="pl--song--content flex items-center">
-      <div class="cover">
-        <img
-          src="https://generative-placeholders.glitch.me/image?width=50&height=50&style=123&colors=25"
-          alt="Song Cover"
-          class="w-10 h-10 rounded-md object-cover"
-        />
+    <div class="pl--song--content flex items-center justify-between">
+      <div class="song--details flex items-center w-3/5">
+        <div class="cover">
+          <img
+            src="https://generative-placeholders.glitch.me/image?width=50&height=50&style=123&colors=25"
+            alt="Song Cover"
+            class="w-10 h-10 rounded-md object-cover"
+          />
+        </div>
+        <div class="title w-2/5">
+          <div class="font-medium text-gray-800">Sundown</div>
+        </div>
+        <div class="artist w-2/5">
+          <div class="font-medium text-gray-400">Gordon Lightfoot</div>
+        </div>
       </div>
-      <div class="title w-1/5">
-        <div class="font-medium text-gray-800">Sundown</div>
+      <div class="link">
+        Link
       </div>
-      <div class="artist w-1/5"></div>
-      <div class="link"></div>
     </div>
   </div>
 </template>
