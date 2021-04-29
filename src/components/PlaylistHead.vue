0bb18a85a9c63c687bd3070612b32c406d26ff23
Deepjyoti Barman <deep.barman30@gmail.com>
2021-04-29 13:14:34 +0530

    Show all the details of the playlist along with a cta button

--- src/components/PlaylistHead.vue
+++ src/components/PlaylistHead.vue
@@ -9,14 +9,21 @@
           class="rounded-md shadow-2xl"
         />
       </div>
-      <div class="pl--details ml-12 my-3">
-        <div class="pl--title text-4xl font-medium dm-sans">My Super Mix</div>
-        <div class="pl--songs text-xl mt-6 text-gray-500 font-medium">
-          254 songs
+      <div class="pl--details ml-12 my-3 flex flex-col justify-between">
+        <div>
+          <div class="pl--title text-4xl text-gray-700 font-medium dm-sans">
+            My Super Mix
+          </div>
+          <div class="pl--songs text-xl mt-6 text-gray-500 font-medium">
+            <span class="text-gray-700 font-semibold">254</span> songs
+          </div>
+          <div class="pl--details mt-3 text-gray-500 roboto max-w-md">
+            Just a bit explaining about the cool playlist that I have made that
+            does not have any resemblance to any other playlist whatsoever.
+          </div>
         </div>
-        <div class="pl--details mt-3 text-gray-500 roboto max-w-md">
-          Just a bit explaining about the cool playlist that I have made that
-          does not have any resemblance to any other playlist whatsoever.
+        <div class="pl--cta">
+          <button>Preview</button>
         </div>
       </div>
     </div>
