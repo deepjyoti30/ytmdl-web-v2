e1ca19adc9f94b7e457101abc6112f4196f1d717
Deepjyoti Barman <deep.barman30@gmail.com>
2021-04-30 13:22:11 +0530

    Make the song list responsive for smaller screens

--- src/components/playlist/PlaylistSong.vue
+++ src/components/playlist/PlaylistSong.vue
@@ -3,20 +3,20 @@
     <div
       class="pl--song--content flex items-center justify-between border-b dark:border-gray-700 py-2"
     >
-      <div class="song--details flex items-center w-3/5">
-        <div class="cover">
+      <div class="song--details flex items-center md:w-3/5 w-full">
+        <div class="cover w-1/6">
           <img
             src="https://generative-placeholders.glitch.me/image?width=50&height=50&style=123&colors=25"
             alt="Song Cover"
             class="w-10 h-10 rounded-md object-cover"
           />
         </div>
-        <div class="title w-2/5">
+        <div class="title md:w-2/5 w-5/6">
           <div class="font-medium text-gray-800 dark:text-gray-300">
             {{ getTitle }}
           </div>
         </div>
-        <div class="artist w-2/5">
+        <div class="artist w-2/5 md:block hidden">
           <div class="font-medium text-gray-400">{{ getArtist }}</div>
         </div>
       </div>
