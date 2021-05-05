e6fd89a88480d31a2389224a97e3175c9e5e72be
Deepjyoti Barman <deep.barman30@gmail.com>
2021-05-05 16:40:14 +0530

    Add init code for the error component

---
+++ src/components/Error.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="error__container">
+    <!--Show the error to the user and indicate what the possible reason might be-->
+    <div class="heading">
+      <h1 class="text-5xl">Damn it!!</h1>
+      <p class="my-8">
+        There was an error with the app. This is probably something we forgot to
+        fix.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Error"
+};
+</script>
