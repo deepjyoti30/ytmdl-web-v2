9727803aef7450397dcc235a5c4e7ce5b38bddea
Deepjyoti Barman <deep.barman30@gmail.com>
2021-05-05 18:34:07 +0530

    Remove the home button and just keep links in the not found page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -2,19 +2,12 @@
   <div class="not--found__container h-screen flex justify-center items-center">
     <div class="wrapper md:w-2/5 w-11/12 mr-auto ml-auto">
       <h1 class="text-5xl text-red-500 dark:text-red-400 font-bold">Oops!</h1>
-      <p class="text-xl text-gray-600 dark:text-gray-400 mt-6">
+      <p class="md:text-xl text-base text-gray-600 dark:text-gray-400 mt-6">
         Seems like you're nowhere. How did you even end up here?!
       </p>
-      <div class="home--btn mt-12 mb-6">
-        <router-link
-          to="/"
-          class="start--btn px-6 py-2 shadow-md font-medium rounded-md bg-darkgreen-300 hover:bg-darkgreen-200 transition duration-150 ease-in"
-          title="Let's go home"
-          >Take me Home</router-link
-        >
-      </div>
       <div class="useful--links mt-6 text-gray-600 dark:text-gray-400">
         <div class="links flex mt-2 roboto underline">
+          <router-link to="/">Home</router-link>
           <router-link to="/about">About</router-link>
           <router-link to="/donate">Donate</router-link>
           <a
