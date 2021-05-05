035715821945ea66f61adece6eda64750249e0bb
Deepjyoti Barman <deep.barman30@gmail.com>
2021-05-05 18:17:12 +0530

    Add init code for not found view

---
+++ src/views/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="not--found__container">
+    <div class="wrapper md:1-2/5 mr-auto ml-auto">
+      <h1 class="text-5xl text-red-600 font-bold">Oops!</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound"
+};
+</script>
