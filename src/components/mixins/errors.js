89c94c8e0a92b005a4768001d8c65b407a177b07
Deepjyoti Barman <deep.barman30@gmail.com>
2021-05-05 17:56:22 +0530

    Add init code to handle an error mixin

---
+++ src/components/mixins/errors.js
@@ -0,0 +1,40 @@
+const error = {
+  data() {
+    return {
+      validCodes: [4, 5]
+    };
+  },
+  methods: {
+    isValidErrorCode: function(code) {
+      /**
+       * Check if the passed error code
+       * is valid.
+       *
+       * We will consider only, 4** and 5**
+       * errors valid.
+       */
+      // We can just divide the code by 100 and
+      // if the number is not 4 or 5, it's not
+      // valid
+      return this.validCodes.includes(code / 100);
+    },
+    extractErrorDetails: function(response) {
+      /**
+       * Extract the error details from the response.
+       *
+       * This method should only be called when the
+       * response is an error (anything other than 20*)?
+       */
+      const code = response.status;
+
+      // Raise error if code is not valid
+      if (!this.isValidErrorCode(code)) throw `${code}: Not a valid error code`;
+
+      // For the message, we will try to extract the
+      // response passed by the API. Else we will fall
+      // back to the generic response.
+    }
+  }
+};
+
+export default error;
