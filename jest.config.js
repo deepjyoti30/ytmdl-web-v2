b3ad60bd5edff400dd8fb1d38025d0524ca8107b
Deepjyoti Barman <deep.barman30@gmail.com>
2021-05-12 18:13:57 +0530

    Move the tests to the tests directory

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest",
-  setupFilesAfterEnv: ["<rootDir>/__tests__/setup.js"],
+  setupFilesAfterEnv: ["<rootDir>/tests/__tests__/setup.js"],
   testPathIgnorePatterns: ["setup.js"]
 };
---
+++ tests/__tests__/metaList.test.js
@@ -0,0 +1,15 @@
+import { shallowMount } from "@vue/test-utils";
+import MetaList from "../src/components/MetaList.vue";
+
+/* eslint-disable no-undef */
+describe("Mounted MetaList", () => {
+  const wrapper = shallowMount(MetaList, {
+    propsData: {
+      query: "Test"
+    }
+  });
+
+  it("should fetch a list", async () => {
+    expect(Array.isArray(wrapper.vm.fetchedData)).toBeTruthy();
+  });
+});
