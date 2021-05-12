043eb105c9b3ce6b15a611fd3be8de11dc1a50e9
Deepjyoti Barman <deep.barman30@gmail.com>
2021-05-12 19:23:57 +0530

    Mock the fetch function in metaList testing using jest-fetch-mock

--- tests/__tests__/metaList.test.js
+++ tests/__tests__/metaList.test.js
@@ -1,15 +1,31 @@
 import { shallowMount } from "@vue/test-utils";
 import MetaList from "../../src/components/MetaList.vue";
+import fetchMock from "jest-fetch-mock";
 
 /* eslint-disable no-undef */
+// Mock fetch
+fetchMock.enableMocks();
+beforeEach(() => {
+  fetch.resetMocks();
+});
+
 describe("Mounted MetaList", () => {
+  fetch.mockResponseOnce(JSON.stringify([{}]));
+
+  const $route = {
+    query: "test"
+  };
   const wrapper = shallowMount(MetaList, {
     propsData: {
-      query: "Test"
+      query: "test"
+    },
+    mocks: {
+      $route
     }
   });
 
   it("should fetch a list", async () => {
+    // /await wrapper.vm.searchMetadata();
     expect(Array.isArray(wrapper.vm.fetchedData)).toBeTruthy();
   });
 });
